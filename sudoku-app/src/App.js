8a73300d9f09d026f3870f46ce2c5aef7cc4ce98
lroland <lroland@redhat.com>
2018-04-27 12:27:50 -0400

    Updates

--- sudoku-app/src/App.js
+++ sudoku-app/src/App.js
@@ -26,12 +26,6 @@ class Game extends Component {
     this.boardRef = React.createRef();
     this.gameStatusRef = React.createRef();
 
-    this.puzzleGrid = Array(9);
-    for (var y=0; y<9; y++) {
-      this.puzzleGrid[y] = Array(9).fill(0);
-    }
-    this.solutionGrid = JSON.parse(JSON.stringify(this.puzzleGrid));
-    this.initialGrid = JSON.parse(JSON.stringify(this.puzzleGrid));
     this.newGame();
   }
 
@@ -43,6 +37,12 @@ class Game extends Component {
 
   newGame() {
     this.setStatus("");
+    this.puzzleGrid = Array(9);
+    for (var y=0; y<9; y++) {
+      this.puzzleGrid[y] = Array(9).fill(0);
+    }
+    this.solutionGrid = JSON.parse(JSON.stringify(this.puzzleGrid));
+    this.initialGrid = JSON.parse(JSON.stringify(this.puzzleGrid));
 
     fetch('http://sudoku-svc-sudoku.apps.home.glroland.com/generate')
     .then(results => {
@@ -56,6 +56,9 @@ class Game extends Component {
       console.log("Initial = " + this.initialGrid);
 
       this.forceUpdate();
+    }).catch(error => {
+      console.log("An error occurred while contacting server for new puzzle: " + error);
+      this.setStatus("Unable to contact server!");
     })
   }
 
@@ -69,9 +72,10 @@ class Game extends Component {
     this.setStatus("");
 
     // compare populated squares to solution
-    for (var y=0; y<9; y++) {
-      for (var x=0; x<9; x++) {
-        if ((this.puzzleGrid[y][x] != 0) && (this.puzzleGrid[y][x] != this.solutionGrid[y][x])) {
+    var x, y;
+    for (y=0; y<9; y++) {
+      for (x=0; x<9; x++) {
+        if ((this.puzzleGrid[y][x] !== 0) && (this.puzzleGrid[y][x] !== this.solutionGrid[y][x])) {
           this.setStatus("Incorrect Value at (" + (x+1) + "," + (y+1) + ")");
           return;
         }
@@ -79,9 +83,9 @@ class Game extends Component {
     }
 
     // ensure its fully populated
-    for (var y=0; y<9; y++) {
-      for (var x=0; x<9; x++) {
-        if (this.puzzleGrid[y][x] == 0) {
+    for (y=0; y<9; y++) {
+      for (x=0; x<9; x++) {
+        if (this.puzzleGrid[y][x] === 0) {
           this.setStatus(this.gameStatusRef.current.value = "Incomplete at (" + (x+1) + "," + (y+1) + ")");
           return;
         }
@@ -94,7 +98,7 @@ class Game extends Component {
   render() {
     return (<div className="game">
       <div className="game-board">
-        <Board ref={this.boardRef} game={this}/>
+        <Board ref={this.boardRef} game={this}/><br/>
         <br/>
         <input ref={this.gameStatusRef} type="text" className="game-status"/><br/>
         <br/>
@@ -122,9 +126,10 @@ class Game extends Component {
     var isValid = true;
 
     // valid horizontal move?
-    for (var ix=0; ix<9; ix++) {
-      if (x != ix) {
-        if (this.puzzleGrid[y][ix] == val) {
+    var ix;
+    for (ix=0; ix<9; ix++) {
+      if (x !== ix) {
+        if (this.puzzleGrid[y][ix] === val) {
           isValid = false;
           break;
         }
@@ -132,9 +137,10 @@ class Game extends Component {
     }
 
     // valid vertical move?
-    for (var iy=0; iy<9; iy++) {
-      if (y != iy) {
-        if (this.puzzleGrid[iy][x] == val) {
+    var iy;
+    for (iy=0; iy<9; iy++) {
+      if (y !== iy) {
+        if (this.puzzleGrid[iy][x] === val) {
           isValid = false;
           break;
         }
@@ -144,10 +150,10 @@ class Game extends Component {
     // valid grid move?
     var gridStartX = x - (x % 3);
     var gridStartY = y - (y % 3);
-    for (var iy=gridStartY; iy<gridStartY+3; iy++) {
-      for (var ix=gridStartX; ix<gridStartX+3; ix++) {
-        if ((x != ix) && (y != iy)) {
-          if (this.puzzleGrid[iy][ix] == val) {
+    for (iy=gridStartY; iy<gridStartY+3; iy++) {
+      for (ix=gridStartX; ix<gridStartX+3; ix++) {
+        if ((x !== ix) && (y !== iy)) {
+          if (this.puzzleGrid[iy][ix] === val) {
             isValid = false;
             break;
           }
@@ -181,104 +187,160 @@ class Board extends Component {
 
   render() {
     return (<div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[0]} index="0" game={this.game} />
-        <Square ref={this.squareRefs[1]} index="1" game={this.game} />
-        <Square ref={this.squareRefs[2]} index="2" game={this.game} />
-        <Square ref={this.squareRefs[3]} index="3" game={this.game} />
-        <Square ref={this.squareRefs[4]} index="4" game={this.game} />
-        <Square ref={this.squareRefs[5]} index="5" game={this.game} />
-        <Square ref={this.squareRefs[6]} index="6" game={this.game} />
-        <Square ref={this.squareRefs[7]} index="7" game={this.game} />
-        <Square ref={this.squareRefs[8]} index="8" game={this.game} />
+      <div className="board-grid-a">
+        <div>
+          <Square ref={this.squareRefs[0]} index="0" game={this.game} />
+          <Square ref={this.squareRefs[1]} index="1" game={this.game} />
+          <Square ref={this.squareRefs[2]} index="2" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[9]} index="9" game={this.game} />
+          <Square ref={this.squareRefs[10]} index="10" game={this.game} />
+          <Square ref={this.squareRefs[11]} index="11" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[18]} index="18" game={this.game} />
+          <Square ref={this.squareRefs[19]} index="19" game={this.game} />
+          <Square ref={this.squareRefs[20]} index="20" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[9]} index="9" game={this.game} />
-        <Square ref={this.squareRefs[10]} index="10" game={this.game} />
-        <Square ref={this.squareRefs[11]} index="11" game={this.game} />
-        <Square ref={this.squareRefs[12]} index="12" game={this.game} />
-        <Square ref={this.squareRefs[13]} index="13" game={this.game} />
-        <Square ref={this.squareRefs[14]} index="14" game={this.game} />
-        <Square ref={this.squareRefs[15]} index="15" game={this.game} />
-        <Square ref={this.squareRefs[16]} index="16" game={this.game} />
-        <Square ref={this.squareRefs[17]} index="17" game={this.game} />
+      <div className="board-grid-b">
+        <div>
+          <Square ref={this.squareRefs[3]} index="3" game={this.game} />
+          <Square ref={this.squareRefs[4]} index="4" game={this.game} />
+          <Square ref={this.squareRefs[5]} index="5" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[12]} index="12" game={this.game} />
+          <Square ref={this.squareRefs[13]} index="13" game={this.game} />
+          <Square ref={this.squareRefs[14]} index="14" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[21]} index="21" game={this.game} />
+          <Square ref={this.squareRefs[22]} index="22" game={this.game} />
+          <Square ref={this.squareRefs[23]} index="23" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[18]} index="18" game={this.game} />
-        <Square ref={this.squareRefs[19]} index="19" game={this.game} />
-        <Square ref={this.squareRefs[20]} index="20" game={this.game} />
-        <Square ref={this.squareRefs[21]} index="21" game={this.game} />
-        <Square ref={this.squareRefs[22]} index="22" game={this.game} />
-        <Square ref={this.squareRefs[23]} index="23" game={this.game} />
-        <Square ref={this.squareRefs[24]} index="24" game={this.game} />
-        <Square ref={this.squareRefs[25]} index="25" game={this.game} />
-        <Square ref={this.squareRefs[26]} index="26" game={this.game} />
+      <div className="board-grid-a">
+        <div>
+          <Square ref={this.squareRefs[6]} index="6" game={this.game} />
+          <Square ref={this.squareRefs[7]} index="7" game={this.game} />
+          <Square ref={this.squareRefs[8]} index="8" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[15]} index="15" game={this.game} />
+          <Square ref={this.squareRefs[16]} index="16" game={this.game} />
+          <Square ref={this.squareRefs[17]} index="17" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[24]} index="24" game={this.game} />
+          <Square ref={this.squareRefs[25]} index="25" game={this.game} />
+          <Square ref={this.squareRefs[26]} index="26" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[27]} index="27" game={this.game} />
-        <Square ref={this.squareRefs[28]} index="28" game={this.game} />
-        <Square ref={this.squareRefs[29]} index="29" game={this.game} />
-        <Square ref={this.squareRefs[30]} index="30" game={this.game} />
-        <Square ref={this.squareRefs[31]} index="31" game={this.game} />
-        <Square ref={this.squareRefs[32]} index="32" game={this.game} />
-        <Square ref={this.squareRefs[33]} index="33" game={this.game} />
-        <Square ref={this.squareRefs[34]} index="34" game={this.game} />
-        <Square ref={this.squareRefs[35]} index="35" game={this.game} />
+
+      <div className="board-grid-b">
+        <div>
+          <Square ref={this.squareRefs[27]} index="27" game={this.game} />
+          <Square ref={this.squareRefs[28]} index="28" game={this.game} />
+          <Square ref={this.squareRefs[29]} index="29" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[36]} index="36" game={this.game} />
+          <Square ref={this.squareRefs[37]} index="37" game={this.game} />
+          <Square ref={this.squareRefs[38]} index="38" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[45]} index="45" game={this.game} />
+          <Square ref={this.squareRefs[46]} index="46" game={this.game} />
+          <Square ref={this.squareRefs[47]} index="47" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[36]} index="36" game={this.game} />
-        <Square ref={this.squareRefs[37]} index="37" game={this.game} />
-        <Square ref={this.squareRefs[38]} index="38" game={this.game} />
-        <Square ref={this.squareRefs[39]} index="39" game={this.game} />
-        <Square ref={this.squareRefs[40]} index="40" game={this.game} />
-        <Square ref={this.squareRefs[41]} index="41" game={this.game} />
-        <Square ref={this.squareRefs[42]} index="42" game={this.game} />
-        <Square ref={this.squareRefs[43]} index="43" game={this.game} />
-        <Square ref={this.squareRefs[44]} index="44" game={this.game} />
+      <div className="board-grid-a">
+        <div>
+          <Square ref={this.squareRefs[30]} index="30" game={this.game} />
+          <Square ref={this.squareRefs[31]} index="31" game={this.game} />
+          <Square ref={this.squareRefs[32]} index="32" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[39]} index="39" game={this.game} />
+          <Square ref={this.squareRefs[40]} index="40" game={this.game} />
+          <Square ref={this.squareRefs[41]} index="41" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[48]} index="48" game={this.game} />
+          <Square ref={this.squareRefs[49]} index="49" game={this.game} />
+          <Square ref={this.squareRefs[50]} index="50" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[45]} index="45" game={this.game} />
-        <Square ref={this.squareRefs[46]} index="46" game={this.game} />
-        <Square ref={this.squareRefs[47]} index="47" game={this.game} />
-        <Square ref={this.squareRefs[48]} index="48" game={this.game} />
-        <Square ref={this.squareRefs[49]} index="49" game={this.game} />
-        <Square ref={this.squareRefs[50]} index="50" game={this.game} />
-        <Square ref={this.squareRefs[51]} index="51" game={this.game} />
-        <Square ref={this.squareRefs[52]} index="52" game={this.game} />
-        <Square ref={this.squareRefs[53]} index="53" game={this.game} />
+      <div className="board-grid-b">
+        <div>
+          <Square ref={this.squareRefs[33]} index="33" game={this.game} />
+          <Square ref={this.squareRefs[34]} index="34" game={this.game} />
+          <Square ref={this.squareRefs[35]} index="35" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[42]} index="42" game={this.game} />
+          <Square ref={this.squareRefs[43]} index="43" game={this.game} />
+          <Square ref={this.squareRefs[44]} index="44" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[51]} index="51" game={this.game} />
+          <Square ref={this.squareRefs[52]} index="52" game={this.game} />
+          <Square ref={this.squareRefs[53]} index="53" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[54]} index="54" game={this.game} />
-        <Square ref={this.squareRefs[55]} index="55" game={this.game} />
-        <Square ref={this.squareRefs[56]} index="56" game={this.game} />
-        <Square ref={this.squareRefs[57]} index="57" game={this.game} />
-        <Square ref={this.squareRefs[58]} index="58" game={this.game} />
-        <Square ref={this.squareRefs[59]} index="59" game={this.game} />
-        <Square ref={this.squareRefs[60]} index="60" game={this.game} />
-        <Square ref={this.squareRefs[61]} index="61" game={this.game} />
-        <Square ref={this.squareRefs[62]} index="62" game={this.game} />
+
+      <div className="board-grid-a">
+        <div>
+          <Square ref={this.squareRefs[54]} index="54" game={this.game} />
+          <Square ref={this.squareRefs[55]} index="55" game={this.game} />
+          <Square ref={this.squareRefs[56]} index="56" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[63]} index="63" game={this.game} />
+          <Square ref={this.squareRefs[64]} index="64" game={this.game} />
+          <Square ref={this.squareRefs[65]} index="65" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[72]} index="72" game={this.game} />
+          <Square ref={this.squareRefs[73]} index="73" game={this.game} />
+          <Square ref={this.squareRefs[74]} index="74" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[63]} index="63" game={this.game} />
-        <Square ref={this.squareRefs[64]} index="64" game={this.game} />
-        <Square ref={this.squareRefs[65]} index="65" game={this.game} />
-        <Square ref={this.squareRefs[66]} index="66" game={this.game} />
-        <Square ref={this.squareRefs[67]} index="67" game={this.game} />
-        <Square ref={this.squareRefs[68]} index="68" game={this.game} />
-        <Square ref={this.squareRefs[69]} index="69" game={this.game} />
-        <Square ref={this.squareRefs[70]} index="70" game={this.game} />
-        <Square ref={this.squareRefs[71]} index="71" game={this.game} />
+      <div className="board-grid-b">
+        <div>
+          <Square ref={this.squareRefs[57]} index="57" game={this.game} />
+          <Square ref={this.squareRefs[58]} index="58" game={this.game} />
+          <Square ref={this.squareRefs[59]} index="59" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[66]} index="66" game={this.game} />
+          <Square ref={this.squareRefs[67]} index="67" game={this.game} />
+          <Square ref={this.squareRefs[68]} index="68" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[75]} index="75" game={this.game} />
+          <Square ref={this.squareRefs[76]} index="76" game={this.game} />
+          <Square ref={this.squareRefs[77]} index="77" game={this.game} />
+        </div>
       </div>
-      <div className="board-row">
-        <Square ref={this.squareRefs[72]} index="72" game={this.game} />
-        <Square ref={this.squareRefs[73]} index="73" game={this.game} />
-        <Square ref={this.squareRefs[74]} index="74" game={this.game} />
-        <Square ref={this.squareRefs[75]} index="75" game={this.game} />
-        <Square ref={this.squareRefs[76]} index="76" game={this.game} />
-        <Square ref={this.squareRefs[77]} index="77" game={this.game} />
-        <Square ref={this.squareRefs[78]} index="78" game={this.game} />
-        <Square ref={this.squareRefs[79]} index="79" game={this.game} />
-        <Square ref={this.squareRefs[80]} index="80" game={this.game} />
+      <div className="board-grid-a">
+        <div>
+          <Square ref={this.squareRefs[60]} index="60" game={this.game} />
+          <Square ref={this.squareRefs[61]} index="61" game={this.game} />
+          <Square ref={this.squareRefs[62]} index="62" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[69]} index="69" game={this.game} />
+          <Square ref={this.squareRefs[70]} index="70" game={this.game} />
+          <Square ref={this.squareRefs[71]} index="71" game={this.game} />
+        </div>
+        <div>
+          <Square ref={this.squareRefs[78]} index="78" game={this.game} />
+          <Square ref={this.squareRefs[79]} index="79" game={this.game} />
+          <Square ref={this.squareRefs[80]} index="80" game={this.game} />
+        </div>
       </div>
     </div>);
   }
@@ -309,13 +371,13 @@ class Square extends Component {
 //    console.log("X=" + x + " Y=" + y + " GPG=" + this.game.puzzleGrid + " L=" + this.game.puzzleGrid.length + " L2=" + this.game.puzzleGrid[0].length);
 
     var value = this.game.puzzleGrid[y][x];
-    if (value == 0) {
+    if (value === 0) {
         value = "";
     }
 
     var isReadOnly = false;
     var cn = this.SQUARE_CLASS_NAME_REG;
-    if (this.game.initialGrid[y][x] != 0) {
+    if (this.game.initialGrid[y][x] !== 0) {
       isReadOnly = true;
       cn = this.SQUARE_CLASS_NAME_LOCKED;
     }
@@ -393,8 +455,8 @@ class GameValuesSelection extends Component {
     var move = 0;
 
     for (var i = 0; i < 9; i++) {
-      if (this.gameValueRefs[i].current.className == this.CLASS_NAME_SEL)
-        move = parseInt(this.gameValueRefs[i].current.innerText);
+      if (this.gameValueRefs[i].current.className === this.CLASS_NAME_SEL)
+        move = parseInt(this.gameValueRefs[i].current.innerText, 10);
     }
 
     console.log("GameValueSection Selected Move = " + move);
