a5ae28bc548561cb0f9fc8dd9cb198042fb9b88a
Lee Roland <lroland@Lees-MBP.home.glroland.com>
2019-04-14 13:30:06 -0400

    Initial framework for python based OCR service

---
+++ sudoku-ocr-svc/ocrsvc/__main__.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+
+app = Flask(__name__)
+api = Api(app)
+
+class Test(Resource):
+    def get(self):
+        return {'results': 0}
+
+        
+
+api.add_resource(Test, '/test')
+
+if __name__ == '__main__':
+     app.run(port='5002')
