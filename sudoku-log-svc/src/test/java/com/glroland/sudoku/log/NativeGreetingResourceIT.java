3185d0477350f55ba52a32b1e2ae6fc39620ce0d
Lee Roland <glroland@hotmail.com>
2021-06-30 20:22:41 -0400

    added template logging service to add game staging also moved deployment config for each service to the home of each service respectively

---
+++ sudoku-log-svc/src/test/java/com/glroland/sudoku/log/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.glroland.sudoku.log;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends LogResourceTest {
+
+    // Execute the same tests but in native mode.
+}
