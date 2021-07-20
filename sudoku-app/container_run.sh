03021d4d3f61d9e2dc1c8d06cd59b7c70733a6a4
Lee Roland <glroland@hotmail.com>
2021-07-19 22:39:16 -0400

    new approach for adding rewrite rules to nginx configuration

--- sudoku-app/container_run.sh
+++ sudoku-app/container_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 envsubst < config.json.env > /tmp/config.json
-envsubst < nginx.conf.env > /tmp/nginx.conf
+envsubst < nginx.conf.env > /opt/app-root/etc/nginx.default.d/nginx.conf
 
 nginx -g "daemon off;"
