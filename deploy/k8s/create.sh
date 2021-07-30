5017c9b87dc7e79b147adb33e89660e9ac9667b6
Lee Roland <glroland@hotmail.com>
2021-07-30 13:07:15 -0400

    warning - the ocp-buildapp is untested
    parameterized create/destroy
    
    added the ocr service to the jenkins build process

--- deploy/k8s/create.sh
+++ deploy/k8s/create.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-kubectl create -f deploy-postgresql.yaml 
-kubectl wait --for=condition=Ready pod --all --timeout=30m
+NAMESPACE=$1
+if [ -z "$NAMESPACE" ]
+then
+      echo "Usage: create.sh <namespace>"
+      exit
+fi
+echo "Using namespace: $NAMESPACE"
 
-kubectl create -f deploy-log-svc.yaml
-kubectl create -f deploy-svc.yaml
+kubectl --namespace=$NAMESPACE create -f deploy-postgresql.yaml 
+kubectl --namespace=$NAMESPACE wait --for=condition=Ready pod --all --timeout=30m
+
+kubectl --namespace=$NAMESPACE create -f deploy-log-svc.yaml
+kubectl --namespace=$NAMESPACE create -f deploy-svc.yaml
--- deploy/k8s/destroy.sh
+++ deploy/k8s/destroy.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-kubectl delete configmap svc-config --cascade='foreground'
-kubectl delete deployment svc --cascade='foreground'
-kubectl delete service svc --cascade='foreground'
+NAMESPACE=$1
+if [ -z "$NAMESPACE" ]
+then
+      echo "Usage: destroy.sh <namespace>"
+      exit
+fi
+echo "Using namespace: $NAMESPACE"
 
-kubectl delete configmap log-svc-config --cascade='foreground'
-kubectl delete deployment log-svc --cascade='foreground'
-kubectl delete service log-svc --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete configmap svc-config --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete deployment svc --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete service svc --cascade='foreground'
 
-kubectl delete deployment postgresql --cascade='foreground'
-kubectl delete secret postgresql --cascade='foreground'
-kubectl delete service postgresql --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete configmap log-svc-config --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete deployment log-svc --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete service log-svc --cascade='foreground'
+
+kubectl --namespace=$NAMESPACE delete deployment postgresql --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete secret postgresql --cascade='foreground'
+kubectl --namespace=$NAMESPACE delete service postgresql --cascade='foreground'
