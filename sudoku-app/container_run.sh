876710d736eb672aa016013d873b2bddcc37d79a
Lee Roland <lroland@lroland-mac.home.glroland.com>
2022-05-17 09:48:25 -0400

    another attempt at fixing npm build issue due to SSL provider in jenkins

--- sudoku-app/container_run.sh
+++ sudoku-app/container_run.sh
@@ -3,4 +3,6 @@
 envsubst < config.json.env > /tmp/config.json
 envsubst < nginx.conf.env > /opt/app-root/etc/nginx.default.d/nginx.conf
 
+export NODE_OPTIONS=--openssl-legacy-provider
+
 nginx -g "daemon off;"
